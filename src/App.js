555a005371ed1e3cd3ce9bb2627d2c48943aca8a
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-08-10 17:39:39 +0000

    Created header section including categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./components/NavBar";
+import ItemCategory from "./components/ItemCategory";
 
 function App() {
   return (
     <>
       <Navbar />
-      
+      <ItemCategory />
     </>
   );
 }
